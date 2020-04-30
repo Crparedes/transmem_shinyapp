04adfa02284efe2ee5ff4b8333a988c355e6b872
Crparedes <craparedesca@unal.edu.co>
2020-04-30 03:48:21 -0500

    table for planar regression included

--- App.R
+++ App.R
@@ -3,13 +3,12 @@ library(shiny)
 library(transmem)
 library(shinydashboard)
 library(rhandsontable)
-
+library(ggplot2)
 
 source('Modules/profileModule.R')
 source('Modules/testModule.R')
 source('Modules/calibrationModule.R')
 source('Modules/configLayouts.R')
-
 
 ui <- dashboardPage(
   dashboardHeader(title = "transmem: Treatment of Membrane-Transport Data", titleWidth = 750),
@@ -18,36 +17,25 @@ ui <- dashboardPage(
   
   dashboardBody(
     tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
-    tabItemsUI,
-    box(title = "Histogram", status = "primary"),
-    tabsetPanel(type = "tabs",
-                tabPanel("Single profile",
-                         multiProfilesUI("Multiprof"),
-                         profileModuleUI("profileModule"))
-    ),
-    hotable("hotable1")
+      tabItemsUI#,
+      #box(title = "Histogram", status = "primary"),
+      #tabsetPanel(type = "tabs",
+      #            tabPanel("Single profile",
+      #                     multiProfilesUI("Multiprof"),
+      #                     profileModuleUI("profileModule"))
+      #)
+    )
   )
-)
-
 
 server <- function(input, output, session) {
-  callModule(testModule, "counter1")
-  #callModule(testModule, "counter2")
+  
   callModule(calibrationModule, "MainSpeciesCal")
+  callModule(calibrationModule, "SeconSpeciesCal")
+  callModule(calibrationModule, "TertiSpeciesCal")
+  
   callModule(multiProfiles, "Multipfor")
   callModule(profileModule, "profileModule")
   
-  # Initiate your table
-  previous <- reactive({data.frame(Conc = rep(0, 6), Signal = rep(0, 6))})
-  MyChanges <- reactive({
-    if(is.null(input$hotable1)){return(previous())}
-    else if(!identical(previous(), input$hotable1)){
-      # hot.to.df function will convert your updated table into the dataframe
-      as.data.frame(hot.to.df(input$hotable1))
-    }
-  })
-  output$hotable1 <- renderHotable({MyChanges()}, readOnly = F)
 }
-
 
 shinyApp(ui, server)
--- Modules/calibrationModule.R
+++ Modules/calibrationModule.R
@@ -11,27 +11,56 @@
 
 calibrationModuleUI <- function(id, inputCM, label.1 = "Main species") {
   ns <- NS(id)
-  box(title = label.1, status = "primary", 
+  box(title = label.1, status = "primary", width = 12,
       selectInput(ns("calModel"), label = 'Model', 
-                  choices = list("Canonical (the data is already in desired units)" = "calCnncl", 
+                  choices = list("Canonical (use when the transport data is already in the desired units)" = "calCnncl", 
                                  "Univariate external standard" = "calUnES",
                                  "Bivariate external standard" = "calBiES",
                                  "Standard addition with dilution" = "calSAWiD",
                                  "Standard addition without dilution (single point)" = "calSAWoD"), 
                   selected = "calCnncl"),
-      conditionalPanel(condition = "input.calModel != 'calCnncl'", ns = ns,
-                       sliderInput(ns("calStdN"), label = "Number of standards", min = 2, max = 12, value = 6),
-                       hotable(ns("CalCurv"))))
+      conditionalPanel(condition = "input.calModel == 'calUnES' || input.calModel == 'calBiES'", ns = ns,
+                       sliderInput(ns("calStdN"), label = "Number of standards", min = 3, max = 12, value = 6)),
+      
+      conditionalPanel(condition = "input.calModel == 'calUnES'", ns = ns,
+                       hotable(ns("ExCalCurv")), 
+                       selectInput(ns("order"), label = 'Model order', 
+                                   choices = list("Linear" = 1, "Quadratic" = 2), selected = 1)#,
+                       #plotOutput(ns('ExCalCurvPlot'))
+                       ),
+      
+      conditionalPanel(condition = "input.calModel == 'calBiES'", ns = ns,
+                       hotable(ns("ExCalPlne"))),
+      
+      conditionalPanel(condition = "input.calModel == 'calSAWiD' || input.calModel == 'calSAWoD'", ns = ns,
+                       h3('Under implementation...')))
 }
 
 calibrationModule <- function(input, output, session) {
-  # Initiate your table
-  previous <- reactive({data.frame(Conc = rep(0, as.numeric(input$calStdN)), Signal = rep(0, as.numeric(input$calStdN)))})
-  MyChanges <- reactive({
-    if(all(as.data.frame(hot.to.df(input$CalCurv)) == 0)){return(previous())}
-    else if(!identical(previous(), input$CalCurv)){
-      as.data.frame(hot.to.df(input$CalCurv)) # hot.to.df function will convert your updated table into the dataframe
+  # External calibration univariate
+  ExCalCurvPrevious <- reactive({data.frame(Conc = rep(0, as.numeric(input$calStdN)), 
+                                            Signal = rep(0, as.numeric(input$calStdN)))})
+  ExCalCurvMyChanges <- reactive({
+    if(all(as.data.frame(hot.to.df(input$ExCalCurv)) == 0)){return(ExCalCurvPrevious())}
+    else if(!identical(ExCalCurvPrevious(), input$ExCalCurv)){
+      as.data.frame(hot.to.df(input$ExCalCurv)) # hot.to.df function will convert your updated table into the dataframe
     }
   })
-  output$CalCurv <- renderHotable({MyChanges()}, readOnly = F)
+  output$ExCalCurv <- renderHotable({ExCalCurvMyChanges()}, readOnly = F)
+  cCurveESU <- reactive({calibCurve(curve = as.data.frame(hot.to.df(input$ExCalCurv)), order = input$order)})
+  output$ExCalCurvPlot <- renderPlot({expr = plot(1:10)})
+  
+  # External calibration bivariate
+  ExCalPlnePrevious <- reactive({data.frame(Conc = rep(0, as.numeric(input$calStdN)), 
+                                            Conc.S = rep(0, as.numeric(input$calStdN)), 
+                                            Signal = rep(0, as.numeric(input$calStdN)))})
+  ExCalPlneMyChanges <- reactive({
+    if(all(as.data.frame(hot.to.df(input$ExCalPlne)) == 0)){return(ExCalPlnePrevious())}
+    else if(!identical(ExCalPlnePrevious(), input$ExCalPlne)){
+      as.data.frame(hot.to.df(input$ExCalPlne)) # hot.to.df function will convert your updated table into the dataframe
+    }
+  })
+  output$ExCalPlne <- renderHotable({ExCalPlneMyChanges()}, readOnly = F)
+  
+  
 }
