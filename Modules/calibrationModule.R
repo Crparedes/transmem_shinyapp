27fe09070519707f3a9b54922c69bcc8fb3ea4c1
Crparedes <craparedesca@unal.edu.co>
2020-05-01 00:26:48 -0500

    bug in input calib data fixed using action button. Inclusion of restart checkbox

--- Modules/calibrationModule.R
+++ Modules/calibrationModule.R
@@ -10,13 +10,19 @@ calibrationModuleUI <- function(id, inputCM, label.1 = "Main species") {
                                    "Standard addition without dilution (single point)" = "calSAWoD"), 
                     selected = "calCnncl"),
         conditionalPanel(condition = "input.calModel == 'calUnES' || input.calModel == 'calBiES'", ns = ns,
-                         sliderInput(ns("calStdN"), label = "Number of standards (modify before entering data)", 
+                         sliderInput(ns("calStdN"), label = "Number of standards (set before entering any data)", 
                                      min = 3, max = 12, value = 6)),
         
         conditionalPanel(condition = "input.calModel == 'calUnES'", ns = ns,
-                         hotable(ns("ExCalCurv")), 
-                         selectInput(ns("order"), label = 'Model order', 
-                                     choices = list("Linear" = 1, "Quadratic" = 2), selected = 1)),
+                         fluidRow(column(5, hotable(ns("ExCalCurv"))),
+                                  column(6, selectInput(ns("order"), label = 'Model order', 
+                                                        choices = list("Linear" = 1, "Quadratic" = 2), selected = 1),
+                                         actionButton(ns("calculateRSC"), label = "Calculate model", 
+                                                      styleclass = 'primary'),
+                                         checkboxInput(ns("restart"), value = FALSE,
+                                                       label = "Restart (This allows changing the number of entries, 
+                                                       unset this box before input data.)"))) 
+                         ),
         
         conditionalPanel(condition = "input.calModel == 'calBiES'", ns = ns,
                          hotable(ns("ExCalPlne"))),
@@ -33,20 +39,21 @@ calibrationModuleUI <- function(id, inputCM, label.1 = "Main species") {
                        tabPanel("Residuals", "Tab content 2")
                      )),
     conditionalPanel(condition = "input.calModel == 'calUnES'", ns = ns,
-                     fluidRow(column(4,
-                                     fluidRow(column(12, infoBox(width = 12, "Calibration function", htmlOutput(ns('niceCurvEq')), 
-                                                                color = 'light-blue', icon = icon("vials")))),
-                                    fluidRow(column(12, infoBox(width = 12, "Statistical significance", htmlOutput(ns('niceStatSg')), 
-                                                               color = 'light-blue', icon = icon("chart-line")))))))
+                     column(4, infoBox(width = 12, "Calibration function", htmlOutput(ns('niceCurvEq')), 
+                                                                color = 'light-blue', icon = icon("vials")),
+                            infoBox(width = 12, "Statistical significance", htmlOutput(ns('niceStatSg')), 
+                                                               color = 'light-blue', icon = icon("chart-line"))))
   )
 }
 
 calibrationModule <- function(input, output, session) {
   # External calibration univariate
   ExCalCurvPrevious <- reactive({data.frame(Conc = rep(0, as.numeric(input$calStdN)), 
-                                            Signal = rep(0, as.numeric(input$calStdN)))})
+                                                 Signal = rep(0, as.numeric(input$calStdN)))})
+  
   ExCalCurvMyChanges <- reactive({
-    ifelse(all(as.data.frame(hot.to.df(input$ExCalCurv)) == 0), 
+    input$calculateRSC
+    ifelse(all(as.data.frame(hot.to.df(input$ExCalCurv)) == 0) || input$restart, 
            return(ExCalCurvPrevious()),
            return(data.frame(apply(as.data.frame(hot.to.df(input$ExCalCurv)), 2, function(x) as.numeric(as.character(x)))))
            ) #hot.to.df function will convert your updated table into the dataframe
@@ -59,66 +66,69 @@ calibrationModule <- function(input, output, session) {
     )
   })
   output$ExCalCurv <- renderHotable({ExCalCurvMyChanges()}, readOnly = F)
-  cCurveESU <- reactive({calibCurve(curve = ExCalCurvMyChanges(), order = as.numeric(input$order), plot = FALSE)})
-  output$niceCurvEq <- renderText({ifelse(as.numeric(input$order) == 1,
-                                          paste0("Signal = (", 
-                                            signif(summary(cCurveESU())$coefficients[1, 1], 4), "\u00b1",
-                                            signif(summary(cCurveESU())$coefficients[1, 2], 2), ") +<br />
+  
+  cCurveESU <- reactive({input$calculateRSC
+    calibCurve(curve = ExCalCurvMyChanges(), order = as.numeric(input$order), plot = FALSE)})
+  reactive_RSC <- eventReactive(input$calculateRSC, {
+    list(Eq = ifelse(as.numeric(input$order) == 1,
+                      paste0("Signal = (", 
+                             signif(summary(cCurveESU())$coefficients[1, 1], 4), "\u00b1",
+                             signif(summary(cCurveESU())$coefficients[1, 2], 2), ") +<br />
                                             &emsp;&emsp;&emsp;&ensp;&ensp;&ensp;(", 
-                                            signif(summary(cCurveESU())$coefficients[2, 1], 4), "\u00b1",
-                                            signif(summary(cCurveESU())$coefficients[2, 2], 2), ") * Conc"),
-                                          paste0("Signal = (", 
-                                                 signif(summary(cCurveESU())$coefficients[1, 1], 4), "\u00b1",
-                                                 signif(summary(cCurveESU())$coefficients[1, 2], 2), ") +<br />
+                             signif(summary(cCurveESU())$coefficients[2, 1], 4), "\u00b1",
+                             signif(summary(cCurveESU())$coefficients[2, 2], 2), ") * Conc"),
+                      paste0("Signal = (", 
+                             signif(summary(cCurveESU())$coefficients[1, 1], 4), "\u00b1",
+                             signif(summary(cCurveESU())$coefficients[1, 2], 2), ") +<br />
                                                  &emsp;&emsp;&emsp;&ensp;&ensp;&ensp;(", 
-                                                 signif(summary(cCurveESU())$coefficients[2, 1], 4), "\u00b1",
-                                                 signif(summary(cCurveESU())$coefficients[2, 2], 2), ") * Conc +<br />
+                             signif(summary(cCurveESU())$coefficients[2, 1], 4), "\u00b1",
+                             signif(summary(cCurveESU())$coefficients[2, 2], 2), ") * Conc +<br />
                                                  &emsp;&emsp;&emsp;&ensp;&ensp;&ensp;(", 
-                                                 signif(summary(cCurveESU())$coefficients[3, 1], 4), "\u00b1",
-                                                 signif(summary(cCurveESU())$coefficients[3, 2], 2), ") * Conc<sup>2</sup>"))
-    
+                             signif(summary(cCurveESU())$coefficients[3, 1], 4), "\u00b1",
+                             signif(summary(cCurveESU())$coefficients[3, 2], 2), ") * Conc<sup>2</sup>")),
+         St = ifelse(as.numeric(input$order) == 1,
+                     paste0("<small>p-values for regression coefficients:</small> <br />&emsp;(Intercept): ", 
+                            round(summary(cCurveESU())$coefficients[1, 4], 5), 
+                            "<br />&emsp;Conc:&emsp;&emsp;&ensp;&nbsp;&nbsp;",
+                            round(summary(cCurveESU())$coefficients[2, 4], 5),
+                            "<hr><small>Residual standard error: ", signif(summary(cCurveESU())$sigma, 4), 
+                            " on ", summary(cCurveESU())$df[2], " DF",
+                            "<br />F-statistic: ", signif(summary(cCurveESU())$fstatistic[1], 4), 
+                            " on ", summary(cCurveESU())$fstatistic[2], " and ", 
+                            summary(cCurveESU())$fstatistic[3], " DF,  p-value: ", 
+                            round(1 - pf(summary(cCurveESU())$fstatistic[1], 
+                                         summary(cCurveESU())$fstatistic[2],
+                                         summary(cCurveESU())$fstatistic[3]), 5), "</small>"),
+                     paste0("<small>p-values for regression coefficients:</small> <br />&emsp;(Intercept): ", 
+                            round(summary(cCurveESU())$coefficients[1, 4], 5), 
+                            "<br />&emsp;Conc:&emsp;&emsp;&ensp;&nbsp;&nbsp;",
+                            round(summary(cCurveESU())$coefficients[2, 4], 5), 
+                            "<br />&emsp;Conc<sup>2</sup>:&emsp;&emsp;&nbsp;&nbsp;",
+                            round(summary(cCurveESU())$coefficients[3, 4], 5),
+                            "<hr><small>Residual standard error: ", signif(summary(cCurveESU())$sigma, 4), 
+                            " on ", summary(cCurveESU())$df[2], " DF",
+                            "<br />F-statistic: ", signif(summary(cCurveESU())$fstatistic[1], 4), 
+                            " on ", summary(cCurveESU())$fstatistic[2], " and ", 
+                            summary(cCurveESU())$fstatistic[3], " DF,  p-value: ", 
+                            round(1 - pf(summary(cCurveESU())$fstatistic[1], 
+                                         summary(cCurveESU())$fstatistic[2],
+                                         summary(cCurveESU())$fstatistic[3]), 5), "</small>")),
+         plt = ggplot(data = ExCalCurvMyChanges(), aes(x = Conc, y = Signal)) +
+           theme_bw() + geom_point(size = 3, shape = 16)  +
+           labs(y = 'Signal', x = expression(paste('Concentration (mg k', g^{-1}, ')'))) +
+           theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                 axis.text.x = element_text(color = "black"),
+                 axis.text.y = element_text(color = "black")) +
+           scale_y_continuous(limits = ExCalCurvXlimYlim()[3:4] + diff(ExCalCurvXlimYlim()[3:4]) * c(-2, 2)) +
+           scale_x_continuous(limits = ExCalCurvXlimYlim()[1:2] + diff(ExCalCurvXlimYlim()[1:2]) * c(-2, 2)) +
+           coord_cartesian(xlim = ExCalCurvXlimYlim()[1:2], ylim = ExCalCurvXlimYlim()[3:4]) +
+           geom_smooth(method = 'lm', formula = y ~ poly(x, as.numeric(input$order)), 
+                       fullrange = TRUE, color = 'black', size = 0.4, level = 0.99))
   })
-  output$niceStatSg <- renderText({ifelse(as.numeric(input$order) == 1,
-                                          paste0("<small>p-values for regression coefficients:</small> <br />&emsp;(Intercept): ", 
-                                                 round(summary(cCurveESU())$coefficients[1, 4], 5), 
-                                                 "<br />&emsp;Conc:&emsp;&emsp;&ensp;&nbsp;&nbsp;",
-                                                 round(summary(cCurveESU())$coefficients[2, 4], 5),
-                                                 "<hr><small>Residual standard error: ", signif(summary(cCurveESU())$sigma, 4), 
-                                                 " on ", summary(cCurveESU())$df[2], " DF",
-                                                 "<br />F-statistic: ", signif(summary(cCurveESU())$fstatistic[1], 4), 
-                                                 " on ", summary(cCurveESU())$fstatistic[2], " and ", 
-                                                 summary(cCurveESU())$fstatistic[3], " DF,  p-value: ", 
-                                                 round(1 - pf(summary(cCurveESU())$fstatistic[1], 
-                                                               summary(cCurveESU())$fstatistic[2],
-                                                               summary(cCurveESU())$fstatistic[3]), 5), "</small>"),
-                                          paste0("<small>p-values for regression coefficients:</small> <br />&emsp;(Intercept): ", 
-                                                 round(summary(cCurveESU())$coefficients[1, 4], 5), 
-                                                 "<br />&emsp;Conc:&emsp;&emsp;&ensp;&nbsp;&nbsp;",
-                                                 round(summary(cCurveESU())$coefficients[2, 4], 5), 
-                                                 "<br />&emsp;Conc<sup>2</sup>:&emsp;&emsp;&nbsp;&nbsp;",
-                                                 round(summary(cCurveESU())$coefficients[3, 4], 5),
-                                                 "<hr><small>Residual standard error: ", signif(summary(cCurveESU())$sigma, 4), 
-                                                 " on ", summary(cCurveESU())$df[2], " DF",
-                                                 "<br />F-statistic: ", signif(summary(cCurveESU())$fstatistic[1], 4), 
-                                                 " on ", summary(cCurveESU())$fstatistic[2], " and ", 
-                                                 summary(cCurveESU())$fstatistic[3], " DF,  p-value: ", 
-                                                 round(1 - pf(summary(cCurveESU())$fstatistic[1], 
-                                                              summary(cCurveESU())$fstatistic[2],
-                                                              summary(cCurveESU())$fstatistic[3]), 5), "</small>"))
-    
-  })
-  output$ExCalCurvPlot <- renderPlot({
-    ggplot(data = ExCalCurvMyChanges(), aes(x = Conc, y = Signal)) +
-      theme_bw() + geom_point(size = 3, shape = 16)  +
-      labs(y = 'Signal', x = expression(paste('Concentration (mg k', g^{-1}, ')'))) +
-      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-            axis.text.x = element_text(color = "black"),
-            axis.text.y = element_text(color = "black")) +
-      scale_y_continuous(limits = ExCalCurvXlimYlim()[3:4] + diff(ExCalCurvXlimYlim()[3:4]) * c(-2, 2)) +
-      scale_x_continuous(limits = ExCalCurvXlimYlim()[1:2] + diff(ExCalCurvXlimYlim()[1:2]) * c(-2, 2)) +
-      coord_cartesian(xlim = ExCalCurvXlimYlim()[1:2], ylim = ExCalCurvXlimYlim()[3:4]) +
-      geom_smooth(method = 'lm', formula = y ~ poly(x, as.numeric(input$order)), 
-                  fullrange = TRUE, color = 'black', size = 0.4, level = 0.99)})
+  output$niceCurvEq <- renderText(reactive_RSC()$Eq)
+  output$niceStatSg <- renderText(reactive_RSC()$St)
+  
+  output$ExCalCurvPlot <- renderPlot(reactive_RSC()$plt)
   
   # External calibration bivariate
   ExCalPlnePrevious <- reactive({data.frame(Conc = rep(0, as.numeric(input$calStdN)), 
@@ -132,3 +142,4 @@ calibrationModule <- function(input, output, session) {
   })
   output$ExCalPlne <- renderHotable({ExCalPlneMyChanges()}, readOnly = F)
 }
+
