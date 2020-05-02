f8e16a595848d7e19acc6300bf2fb6b208f0874f
Crparedes <craparedesca@unal.edu.co>
2020-05-01 22:31:08 -0500

    implemented normalization in datainput

---
+++ Modules/inputDataModule.R
@@ -0,0 +1,84 @@
+inputDataModuleUI <- function(id, IntID = 1, Spc = "Main species", Model) {
+  ns <- NS(id)
+  box(title = paste0("System ", IntID, " - ", Spc), width = 4, 
+      sliderInput(ns("nData"), label = "Number of aliquots (set table values to zero to modify)", 
+                  min = 3, max = 30, value = 6),
+      #hotable(ns("TrnsDt")),
+      #tableOutput(ns("TrnsfrmdDt")))
+      column(12, column(8, uiOutput(ns("chckbx"))),
+      column(4, h5(tags$b('Settings')), checkboxInput(ns("nrmliz"), label = "Normalize data", value = TRUE))),
+      column(5, hotable(ns("TrnsDt"))),
+      column(7, textOutput(ns('ModelMsg')),
+             #uiOutput(ns("chckbx")),
+             uiOutput(ns("button")),
+             tableOutput(ns("TrnsfrmdDt"))))
+}
+
+inputDataModule <- function(input, output, session, Model, ...) {
+    chcMdl <- reactive((1:4)[c(c('calCnncl', 'calUnES', 'calBiES', 'calSAWoD') == Model$natModel())])
+  messag <- c('No transformation will be applied', rep('Be sure the calibration data has been provided', 2),
+              'Transformation using bivariated external calibration', 'Transformation using single point standard addition method')
+  output$ModelMsg <- renderText({messag[chcMdl()]})
+  output$chckbx <- renderUI(if(as.numeric(chcMdl()) != 1) {
+    checkboxGroupInput(session$ns("chckbx"), label = 'Aliquots diluted before measurement?', 
+                       choices = list("Feed" = 1, "Strip" = 2))})
+
+  output$button <- renderUI(actionButton(session$ns('button'), label = c('Input data', rep('Transform data', 3))[chcMdl()], 
+                                         styleclass = 'primary'))
+  #dilutBool <- reactive({list(input$chckbx[[1]], input$chckbx[[2]])})
+  
+  TrnsDtPrevious <- reactive({
+    if (chcMdl() == 1 || is.null(input$chckbx)) {#|| !(input$chckbx[[1]] || input$chckbx[[2]])) {
+      data.frame(Time  = rep(0, as.numeric(input$nData)), 
+                 Feed  = rep(0, as.numeric(input$nData)),
+                 Strip = rep(0, as.numeric(input$nData)))
+    } else {
+      if (all(input$chckbx == 1:2)) {
+          data.frame(Time  = rep(0, as.numeric(input$nData)), 
+                     Feed  = rep(0, as.numeric(input$nData)),
+                     Dil.Feed  = rep(0, as.numeric(input$nData)),
+                     Strip = rep(0, as.numeric(input$nData)),
+                     Dil.Strip  = rep(0, as.numeric(input$nData)))
+      } else {
+        if (input$chckbx == 1) {
+          data.frame(Time  = rep(0, as.numeric(input$nData)), 
+                     Feed  = rep(0, as.numeric(input$nData)),
+                     Dil.Feed  = rep(0, as.numeric(input$nData)),
+                     Strip = rep(0, as.numeric(input$nData)))
+        } else {
+          data.frame(Time  = rep(0, as.numeric(input$nData)), 
+                     Feed  = rep(0, as.numeric(input$nData)),
+                     Strip = rep(0, as.numeric(input$nData)),
+                     Dil.Strip  = rep(0, as.numeric(input$nData)))
+        }
+      }
+    } 
+  })
+  TrnsDtMyChanges <- reactive({
+    TempDatFram1 <- data.frame(apply(as.data.frame(hot.to.df(input$TrnsDt)), 2, function(x) as.numeric(as.character(x))))
+    ifelse(all(as.data.frame(hot.to.df(input$TrnsDt)) == 0), 
+           return(TrnsDtPrevious()),
+           return(TempDatFram1)
+    ) #hot.to.df function will convert your updated table into the dataframe
+  })
+  output$TrnsDt <- renderHotable({TrnsDtMyChanges()}, readOnly = F)
+  
+  #transDat <- reactive(TrnsDtMyChanges()
+  datTabReactive <- eventReactive(input$button, {
+    TempDatFram2 <- data.frame(apply(as.data.frame(hot.to.df(input$TrnsDt)), 2, function(x) as.numeric(as.character(x))))
+    #ifelse(chcMdl() == 1, return(TempDatFram2),
+    #       ifelse(chcMdl() == 2, return(data.frame(Time = as.data.frame(TrnsDtMyChanges())$Time,
+    #                                               Feed = signal2conc(signal = as.data.frame(TrnsDtMyChanges())$Feed, 
+    #                                                                  model = Model$Model()),
+    #                                               Strip = signal2conc(signal = as.data.frame(TrnsDtMyChanges())$Strip, 
+    #                                                                   model = Model$Model())))))
+    mxfd <- max(TempDatFram2$Feed)
+    ifelse(input$nrmliz, return(data.frame(Time = TempDatFram2$Time, 
+                                           Feed = TempDatFram2$Feed/mxfd,
+                                           Strip = TempDatFram2$Strip/mxfd)),
+           return(TempDatFram2))
+  }
+    
+  )
+  output$TrnsfrmdDt <- renderTable(datTabReactive())
+}
